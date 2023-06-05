658a06869e2d1aef453b7dcdf4fa20a5ce1d4b91
WitBlue6 <107780822+WitBlue6@users.noreply.github.com>
2023-06-05 12:42:16 +0800

    Add files via upload

--- Core/love.py
+++ Core/love.py
@@ -9,8 +9,8 @@ class MainPage:
 
         self.left = 0
         self.right = 2  # 记录按钮左右位置
-
-        self.words = ["不要拒绝人家嘛~", "真的不再考虑一下嘛~", "不喜欢我嘛~", "不要这样嘛~", "你就从了我吧~",
+        self.flag = False # 记录是否拒绝过
+        self.words = ["不要拒绝人家嘛~",  "真的不再考虑一下嘛~", "不喜欢我嘛~", "不要这样嘛~", "你就从了我吧~",
                       "来嘛来嘛~", "快说你喜欢人家啦~", "不要点这个选项了嘛~", "好不好嘛~", "哎哟~", "同意人家好不好嘛~"]
 
         self.root.overrideredirect(True)
@@ -19,11 +19,16 @@ class MainPage:
     def show_button(self):
         self.frame = tk.Frame()
         tk.Label(self.frame).grid(row=0)
-        tk.Label(self.frame, text='你喜欢我吗^_^', height=3, font=18, justify='center', fg='blue').grid(row=1, column=1)
+        if self.flag == False:
+            tk.Label(self.frame, text='你喜欢我吗^_^', height=3, font=18, justify='center', fg='blue').grid(row=1, column=1)
+        else:
+            tk.Label(self.frame, text=self.words[rd.randint(0, len(self.words)-1)], height=3,
+                     font=18, justify='center', fg='green').grid(row=1, column=1)
         tk.Label(self.frame).grid(row=2)
 
         tk.Button(self.frame, text='喜欢', height=2, font=10, command=self.like, fg='red').grid(row=3, column=self.left)
         tk.Button(self.frame, text='不喜欢', height=2, font=10, command=self.dislike, fg='red').grid(row=3, column=self.right)
+
         self.frame.pack()
 
     def like(self):
@@ -35,7 +40,9 @@ class MainPage:
         tmp = self.left
         self.left = self.right
         self.right = tmp
-        messagebox.showinfo(title='哎呀', message=self.words[rd.randint(0, len(self.words))])
+
+        self.flag = True  # 记录是否不喜欢过
+
         self.show_button()
 
 if __name__ == '__main__':
